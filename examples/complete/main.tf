f84921851ac82f2b5a59cdd8dfa16559a9276f49
James Cooke <jamesedwardcooke@gmail.com>
2021-08-19 10:56:02 -0500

    Switch to k8s provider. Resolve issue with values as map

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,30 +1,29 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "k3d-tftest"
-}
 
-provider "kubernetes-alpha" {
-  config_path    = "~/.kube/config"
-  config_context = "k3d-tftest"
+  experiments {
+    manifest_resource = true
+  }
 }
 
 module "flux-install" {
   count   = 1
   source  = "L2Solutions/install/flux"
-  version = "0.0.26"
+  version = ">= 0.0.27"
 }
 
 # Set to true after flux-install. GitRepository CRD need to be created before the repo instances may be created.
 locals {
-  install_complete = true
-  # install_complete = false
+  install_complete = false
 }
+
 
 module "helm-repository" {
   count = local.install_complete ? 1 : 0
 
   source  = "L2Solutions/helm-repository/flux"
-  version = "0.0.5"
+  version = "0.0.6"
 
   name = "helm-repository-bitnami"
   url  = "https://charts.bitnami.com/"
@@ -45,23 +44,25 @@ module "helm-release-helm" {
   depends_on = [module.helm-repository]
 }
 
-# module "git-repository" {
-#   count = local.install_complete ? 1 : 0
+module "git-repository" {
+  count = local.install_complete ? 1 : 0
 
-#   source  = "L2Solutions/git-repository/flux"
-#   version = "0.0.3"
+  source  = "L2Solutions/git-repository/flux"
+  version = "0.0.4"
 
-#   name = "helm-release-test"
-#   url  = "https://github.com/L2Solutions/helm-chart-example.git"
+  name = "helm-release-test"
+  url  = "https://github.com/L2Solutions/helm-chart-example.git"
 
-#   # This will prevent a condition where the namespace cannot be removed if a CR for a CRD still exists.
-#   depends_on = [module.flux-install]
-# }
+  # This will prevent a condition where the namespace cannot be removed if a CR for a CRD still exists.
+  depends_on = [module.flux-install]
+}
 
-# module "helm-release-git" {
-#   count = local.install_complete ? 1 : 0
+module "helm-release-git" {
+  count = local.install_complete ? 1 : 0
 
-#   source = "../../"
+  source = "../../"
 
-#   name = "helm-release-git"
-# }
+  name        = "helm-release-git"
+  chart       = "chart"
+  source_name = "helm-release-test"
+}
